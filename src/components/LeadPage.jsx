751808f45b339705ed5387136d8f403692f61ac5
ValeryManoilov <manojlovv@inbox.ru>
2025-04-07 19:44:49 +0300

    test 35

--- src/components/LeadPage.jsx
+++ src/components/LeadPage.jsx
@@ -66,7 +66,6 @@ const CookieImage = styled.img`
   transition: all 0.4s;
   background-color: #5db3fe;
   animation: ${rotateAnimation} ${props => props.RotateSpeed} linear infinite;
-  filter: drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.3));
 `
 
 const Title = styled.h1`
