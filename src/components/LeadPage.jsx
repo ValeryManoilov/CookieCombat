e1585d73b5b4e0d9505dc00b3b97addc5296a38b
ValeryManoilov <manojlovv@inbox.ru>
2025-04-07 19:34:26 +0300

    test 30

--- src/components/LeadPage.jsx
+++ src/components/LeadPage.jsx
@@ -51,7 +51,7 @@ const Scores = styled.p`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -150%);
+  transform: translate(-50%, -50%);
   font-size: 5em;
 
   text-shadow: #fc0 1px 0 10px;
